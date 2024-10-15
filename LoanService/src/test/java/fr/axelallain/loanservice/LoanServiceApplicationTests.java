4feb61fc9e6dc152d15d6088983f21ec065b1131
axelallain <axel.allain.a@gmail.com>
2024-10-15 22:17:45 +0200

    Create LoanService.

---
+++ LoanService/src/test/java/fr/axelallain/loanservice/LoanServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.axelallain.loanservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
